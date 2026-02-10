9f1f030dcd4a7e9e1937dd63090dcdea2df6fec5
BurnedVegetable <pitikarn.k@hotmail.com>
2026-02-10 22:32:50 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // ========= CONFIG =========
 const CLIENT_ID = "138952700999-cqqv535u827d2vkfneg0vtfldrucfulq.apps.googleusercontent.com";
 const API_KEY = "sheets.googleapis.com";
-const SHEET_ID = "https://docs.google.com/spreadsheets/d/1mDME0-n2I9mEe9H_eNUAcEmuq4Z4rJSIdasPuZ0ct7E/edit?usp=sharing";
+const SHEET_ID = "https://docs.google.com/spreadsheets/d/1mDME0-n2I9mEe9H_eNUAcEmuq4Z4rJSIdasPuZ0ct7E/edit";
 
 const SCOPES = "https://www.googleapis.com/auth/spreadsheets";
 
@@ -100,4 +100,5 @@ document.getElementById("btnDashboard").onclick = async () => {
     }
   });
 };
+
 
